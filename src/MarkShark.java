ff92f0d7564775b3a43d1b76a49cfba036c30bd5
Colin Woodbury <colingw@gmail.com>
2014-03-22 14:50:06 -0700

    Added main class - MarkShark.java

---
+++ src/MarkShark.java
@@ -0,0 +1,9 @@
+/*
+ * This should be the only .java file on this level.
+ */
+
+public class MarkShark {
+    public static void main(String doesnt_matter[]) {
+	System.out.println("Operaition Complete.");
+    }
+}
