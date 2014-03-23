0fece8a8bb377bb939075246bcdad40394a43b2f
Colin Woodbury <colingw@gmail.com>
2014-03-23 16:05:37 -0700

    Removed some verbosity

--- src/gui/MarkSharkGUI.java
+++ src/gui/MarkSharkGUI.java
@@ -3,6 +3,7 @@
 package gui;
 
 import javax.swing.*;
+import java.util.logging.*;
 
 public class MarkSharkGUI {
     private LoginScreen login;
@@ -15,17 +16,17 @@ public class MarkSharkGUI {
          try {
              UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
          } catch (ClassNotFoundException ex) {
-             java.util.logging.Logger.getLogger(LoginScreen.class.getName())
-		 .log(java.util.logging.Level.SEVERE, null, ex);
+             Logger.getLogger(LoginScreen.class.getName())
+		 .log(Level.SEVERE, null, ex);
          } catch (InstantiationException ex) {
-             java.util.logging.Logger.getLogger(LoginScreen.class.getName())
-		 .log(java.util.logging.Level.SEVERE, null, ex);
+             Logger.getLogger(LoginScreen.class.getName())
+		 .log(Level.SEVERE, null, ex);
          } catch (IllegalAccessException ex) {
-             java.util.logging.Logger.getLogger(LoginScreen.class.getName())
-		 .log(java.util.logging.Level.SEVERE, null, ex);
-         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-             java.util.logging.Logger.getLogger(LoginScreen.class.getName())
-		 .log(java.util.logging.Level.SEVERE, null, ex);
+             Logger.getLogger(LoginScreen.class.getName())
+		 .log(Level.SEVERE, null, ex);
+         } catch (UnsupportedLookAndFeelException ex) {
+             Logger.getLogger(LoginScreen.class.getName())
+		 .log(Level.SEVERE, null, ex);
          }
 
 	 // Display the Login Page
